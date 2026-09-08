262c7f7525eeffb0d8f942e3efbaf078fd3bd433
Tesigit <kolusutejaswi99@gmail.com>
2026-09-08 09:46:00 +0530

    Fix CSS import path for ProfilePage component

--- Ecommerce-app/src/components/ProfilePage.jsx
+++ Ecommerce-app/src/components/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "./ProfilePage.css"; // import the CSS
+import "./Profilepage.css"; // import the CSS
 
 const ProfilePage = () => {
   const [userDetails, setUserDetails] = useState(null);
